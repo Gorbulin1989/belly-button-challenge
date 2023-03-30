6ba5feb9a9fca9319816c6e74e7e06bfed1d7512
Gorbulin1989 <116699108+Gorbulin1989@users.noreply.github.com>
2023-03-30 17:50:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 var dropdown = d3.select("#selDataset");
 
 // Load the data from your JSON file
-d3.json("d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function(data) {
-").then(function(data) {
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function(data) {
 
   // Get the first subject ID from the names array
   var subjectID = data.names[0];
